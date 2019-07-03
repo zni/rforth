ed3d46f22f036288ed841eeb39abcee4d4618123
Matt Godshall <lifeinhex@gmail.com>
2019-07-02 21:09:48 -0400

    Add weird broken hacked up compilation mode.

--- src/vm/machine.rs
+++ src/vm/machine.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 use crate::vm::instructions;
 use crate::vm::ErrorType;
@@ -9,7 +10,16 @@ pub enum Function {
     UserDefined(Vec<Value>)
 }
 
+impl fmt::Debug for Function {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Function")
+    }
+}
+
+#[derive(Debug)]
 pub struct Machine {
+    pub compile_mode: bool,
+    pub compile_buffer: Vec<Value>,
     pub dictionary: HashMap<String, Function>,
     pub stack: Vec<i32>
 }
@@ -17,6 +27,8 @@ pub struct Machine {
 impl Machine {
     pub fn new() -> Machine {
         let mut dictionary = HashMap::new();
+        dictionary.insert(String::from(":"), Function::Builtin(compile));
+        dictionary.insert(String::from(";"), Function::Builtin(finish_compile));
         dictionary.insert(String::from("+"), Function::Builtin(instructions::add));
         dictionary.insert(String::from("-"), Function::Builtin(instructions::sub));
         dictionary.insert(String::from("*"), Function::Builtin(instructions::mult));
@@ -32,6 +44,8 @@ impl Machine {
         dictionary.insert(String::from(">"), Function::Builtin(instructions::greater_than));
         dictionary.insert(String::from("<"), Function::Builtin(instructions::less_than));
         Machine {
+            compile_mode: false,
+            compile_buffer: Vec::new(),
             dictionary,
             stack: Vec::new()
         }
@@ -45,41 +59,18 @@ impl Machine {
         self.stack.pop()
     }
 
-    pub fn compile(&mut self, line: &String) -> Result<(), ErrorType> {
-        let mut words = line.split_whitespace();
-
-        // Skip ':'.
-        words.next();
-
-        // Get definition name.
-        let name = match words.next() {
-            Some(n) => n.to_string(),
-            None => return Err(ErrorType::CompilationError)
-        };
-
-        let mut definition: Vec<Value> = Vec::new();
-        for word in words {
-            if word == ";" {
-                break;
+    pub fn execute(&mut self, value: &Value) -> Result<(), ErrorType> {
+        if let Value::Word(s) = value {
+            if s == ";" {
+                self.compile_mode = false;
             }
-
-            let token = match word.parse::<i32>() {
-                Ok(num) => {
-                    Value::Number(num)
-                },
-                Err(_) => {
-                    Value::Word(word.to_string())
-                }
-            };
-            definition.push(token);
         }
 
-        self.dictionary.insert(name, Function::UserDefined(definition));
-        Ok(())
-    }
+        if self.compile_mode {
+            self.compile_buffer.push(value.clone());
+            return Ok(());
+        }
 
-
-    pub fn execute(&mut self, value: &Value) -> Result<(), ErrorType> {
         let word = match value {
             Value::Number(n) => {
                 self.push(*n);
@@ -111,4 +102,28 @@ impl Machine {
 
         return Ok(());
     }
+}
+
+fn compile(machine: &mut Machine) -> Result<(), ErrorType> {
+    machine.compile_mode = true;
+    Ok(())
+}
+
+fn finish_compile(machine: &mut Machine) -> Result<(), ErrorType> {
+    machine.compile_mode = false;
+
+    let mut word;
+    if machine.compile_buffer.len() > 0 {
+        word = match machine.compile_buffer.remove(0) {
+            Value::Number(n) => n.to_string(),
+            Value::Word(w) => w
+        };
+    } else {
+        return Err(ErrorType::CompilationError);
+    }
+
+    machine.dictionary.insert(word.clone(), Function::UserDefined(machine.compile_buffer.clone()));
+
+    machine.compile_buffer.clear();
+    Ok(())
 }
